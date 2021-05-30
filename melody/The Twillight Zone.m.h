fc8ba2aaa2426af4ffa6edbaa3bd09f203910708
Ricky Gai <rickygai@yahoo.com>
2021-05-30 13:00:44 +0800

    Tone Studio C/C++ melody header files.

---
+++ melody/The Twillight Zone.m.h
@@ -0,0 +1,11 @@
+
+#ifndef The_Twillight_Zone_H_
+#define The_Twillight_Zone_H_
+#include "pitch.h"
+_MELODY The_Twillight_Zone[]=
+{
+{523.25, 128},{554.37, 128},{587.33, 128},
+{1567.98, 128},{1479.98, 128},{415.30, 128},
+{440.00, 128},{466.16, 128}
+};
+#endif // The_Twillight_Zone_H_
